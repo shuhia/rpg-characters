9a2e280b1c77acb69c78b06f34abdfcacea964da
Alex On <shuhia.on@gmail.com>
2022-02-10 17:18:30 +0100

    feat(CharacterSpecification): add abstract class

---
+++ src/com/company/CharacterSpecifications.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import static com.company.ArmorType.*;
+import static com.company.WeaponType.*;
+
+public abstract class CharacterSpecifications {
+    // Mage specifications
+    public static Attributes mageBaseAttributes = new Attributes(1, 1, 8);
+    public static Attributes mageGrowthAttributes = new Attributes(1, 1, 5);
+    public static ArmorType[] mageArmorTypes = {CLOTH};
+    public static WeaponType[] mageWeaponTypes = {STAFF, WAND};
+
+    // Ranger specifications
+    public static Attributes rangerBaseAttributes = new Attributes(1, 5, 1);
+    public static Attributes rangerGrowthAttributes = new Attributes(1, 5, 1);
+    public static WeaponType[] rangerWeaponTypes = {BOW};
+    public static ArmorType[] rangerArmorTypes = {LEATHER, MAIL};
+
+    // Rogue specifications
+    public static Attributes rougeBaseAttributes = new Attributes(2, 6, 1);
+    public static Attributes rougeGrowthAttributes = new Attributes(1, 4, 1);
+    public static WeaponType[] rogueWeaponTypes = {DAGGER, SWORD};
+    public static ArmorType[] rogueArmorTypes = {LEATHER, MAIL};
+
+    // Warrior specifications
+    public static Attributes warriorBaseAttributes = new Attributes(5, 2, 1);
+    public static Attributes warriorGrowthAttributes = new Attributes(3, 2, 1);
+    public static WeaponType[] warriorWeaponTypes = {AXE, HAMMER, SWORD};
+    public static ArmorType[] warriorArmorTypes = {MAIL, PLATE};
+
+}
