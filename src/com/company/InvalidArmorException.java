03a1f8c4b42d3afe4cd96639974acadea3875076
Alex On <shuhia.on@gmail.com>
2022-02-10 17:19:27 +0100

    feat(Expections): add InvalidArmorException and InvalidWeaponException

---
+++ src/com/company/InvalidArmorException.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class InvalidArmorException extends Exception {
+    public InvalidArmorException(String message) {
+        super(message);
+    }
+}
---
+++ src/com/company/InvalidWeaponException.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class InvalidWeaponException extends Exception {
+    public InvalidWeaponException(String message) {
+        super(message);
+    }
+}
